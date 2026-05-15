587824fceda6c135edeef39e7b032764708fb848
Uindow <181064963+uindow@users.noreply.github.com>
2026-05-15 18:13:02 +0300

    [code] Removed dependency to deprecated asar package

--- tools/build-hooks/after-pack.js
+++ tools/build-hooks/after-pack.js
@@ -16,9 +16,9 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-const asar = require("asar");
-const path = require("path");
+const asar = require("@electron/asar");
 const fs = require("fs-extra");
+const path = require("path");
 
 /**
  * @param {Object} context Context
